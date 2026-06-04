986942a7f4571e770e8ee39b71a9d8b8e3537695
Kelechi <dialakelechi13@gmail.com>
2026-06-04 17:05:13 +0100

    fix contact js syntax

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -18,8 +18,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: 465,
 secure: true,
-  secure: false,
-  auth: 
+  auth: { 
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
