f4d36356c5e6877b40e7068dd90af40a7495ca3e
Kelechi <dialakelechi13@gmail.com>
2026-06-22 17:14:49 +0100

    debug step 1

--- public/js/main.js
+++ public/js/main.js
@@ -23,7 +23,7 @@ window.onload = function() {
     form.onsubmit = function(e) {
       e.preventDefault();
       e.stopPropagation();
-      alert("Form submitted! Name: " + document.getElementById("name").value);
+      alert("step 1");
 
       var name = document.getElementById("name").value.trim();
       var email = document.getElementById("email").value.trim();
